29f3140b9254ec49320f70e028e44c896037eb41
kathir2602 <kathir@crayond.co>
2022-02-01 22:01:00 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Review from './Review';
+
+const url = 'https://jsonplaceholder.typicode.com/users';
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review users={data} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    id: 1,
+    name: 'kathir',
+    job: 'web developer',
+    image: 'https://random.imagecdn.app/500/150',
+    text: 'lorem ispum',
+  },
+  {
+    id: 2,
+    name: 'Naveen',
+    job: 'web designer',
+    image: 'https://picsum.photos/200/300',
+    text: 'lorem ispum',
+  },
+  {
+    id: 3,
+    name: 'Sreeman',
+    job: 'intern',
+    image: 'https://picsum.photos/200/300',
+    text: 'lorem ispum',
+  },
+  {
+    id: 4,
+    name: 'Yuvaraj',
+    job: 'the boss',
+    image: 'https://picsum.photos/200/300',
+    text: 'lorem ispum',
+  },
+];
+
+export default users;
